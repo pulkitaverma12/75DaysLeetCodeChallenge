db81a28a4688ab7f85969e336114c9d64244448b
PULKITA VERMA <pulkitaverma10@gmail.com>
2026-03-19 22:36:15 +0530

    Time: 3 ms (30.43%), Space: 9.7 MB (93.71%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            // skip non-alphanumeric
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            // compare lowercase
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
