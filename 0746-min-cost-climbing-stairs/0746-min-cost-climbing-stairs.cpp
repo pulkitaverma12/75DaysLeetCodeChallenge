d60519e0f74861906b8ef4011fa3e91b9efd751a
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2026-06-02 22:59:00 +0530

    Time: 0 ms (100%), Space: 12 MB (29.01%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n1=cost.size();
-        for(int i=2;i<n1;i++)
+        int n=cost.size();
+        for(int i=2;i<n;i++)
         {
             cost[i]+=min(cost[i-1],cost[i-2]);
         }
-        return min(cost[n1-1],cost[n1-2]);
+        return min(cost[n-1],cost[n-2]);
     }
 };
