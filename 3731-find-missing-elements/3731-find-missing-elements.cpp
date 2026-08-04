029355fb00f9cf6648e452dcc314f93188af7a45
PULKITA VERMA <pulkitaverma10@gmail.com>
2026-08-04 19:24:26 +0530

    Time: 3 ms (49.33%), Space: 32.3 MB (75.54%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        vector<int> ans;
+        vector<int> a;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 1; i++) {
             while (nums[i] + 1 < nums[i + 1]) {
-                ans.push_back(nums[i] + 1);
+                a.push_back(nums[i] + 1);
                 nums[i]++;
             }
         }
-        return ans;
+        return a;
     }
 };
