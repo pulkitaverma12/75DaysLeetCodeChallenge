1aad257c5884845c7739b5b59c86c2c2c78314fd
PULKITA VERMA <pulkitaverma10@gmail.com>
2026-09-26 17:56:24 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.63%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        bool iscap = true;
-        bool issmall = true;
-        for(int i = 0; i < word.size(); i++) {
-            if(islower(word[i])) {
-                iscap = false;
-            }
-            if(isupper(word[i])) {
-                issmall = false;
-            }
+        int cap = 0;
+        for(int i = 0 ;i < word.length(); i++){
+            if(isupper(word[i])) cap++;
         }
-        bool firstCapital = isupper(word[0]);
-        for(int i = 1; i < word.size(); i++) {
-            if(isupper(word[i])) {
-                firstCapital = false;
-            }
-        }
-        if(iscap || issmall || firstCapital) {
-            return true;
-        }
-        return false;
+        if(cap == 1 && isupper(word[0])) return true;
+        else if(cap == 0) return true;
+        else if(cap == word.length()) return true;
+        return false; 
     }
 };
