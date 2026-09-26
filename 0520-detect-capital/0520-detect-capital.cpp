8e883b24a7e9be958723b740eb81fedec90bc6e0
PULKITA VERMA <pulkitaverma10@gmail.com>
2026-09-26 17:58:26 +0530

    Time: 0 ms (100%), Space: 8 MB (79.18%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -5,8 +5,11 @@ public:
         for(int i = 0 ;i < word.length(); i++){
             if(isupper(word[i])) cap++;
         }
-        if(cap == 1 && isupper(word[0])) return true;
+        // agar vo capital hone ka count 1 hai or 1st index me hi cap hai baki small toh
+        if(cap == 1 && isupper(word[0])) return true;  
+        // agar capital count 0 hai or small hogya sb 
         else if(cap == 0) return true;
+        // agar captial count word ke length jitna hai matlab sab capital
         else if(cap == word.length()) return true;
         return false; 
     }
